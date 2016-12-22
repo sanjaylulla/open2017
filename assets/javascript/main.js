fe7c3255a15b82d131c9e68bf1618223b53cfcf2
ErikUu <erik.uusitalo@gmail.com>
2016-12-22 14:53:13 +0100

    JS small clean-up

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -42,7 +42,8 @@ $(window).on('resize', resizeIframe);
 resizeIframe();
 
 // Twitter timeline
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+!function(d,s,id){
+    var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
     if(!d.getElementById(id)){
         js=d.createElement(s);js.id=id;
         js.src=p+"://platform.twitter.com/widgets.js";
